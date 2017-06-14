3569ed5e54766a6df0b59d86f31a4213e6627167
tat08507 <ajayreddyyeruva@gmail.com>
2017-06-14 11:47:06 -0700

    version1

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
 
-name             'kafka Test'
+name             'kafka'
 maintainer       'Mathias'
 maintainer_email 'mths@sdrbrg.se'
 license          'Apache 2.0'
 description      'Installs and configures a Kafka broker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.2.1'
+version          '2.2.2'
 
 recipe 'kafka::default', 'Downloads and installs Kafka from binary releases'
 
